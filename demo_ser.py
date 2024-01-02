bad32cd535bae68fbf0c72d4dbd1ec520d3fc846
manu <zxthz@126.com>
2024-01-02 13:07:31 +0800

    tiny

--- demo_ser.py
+++ demo_ser.py
@@ -9,12 +9,13 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # parser.add_argument('--addrs_sensor', default=[f'1_{1}', f'1_{5}'])
     parser.add_argument('--addrs_sensor', default=[f'1_{2}'])
-    # parser.add_argument('--dir_root_libsvm', default='/home/manu/nfs/libsvm')
-    # parser.add_argument('--dev_ser', default='/dev/ttyUSB0')
-    # parser.add_argument('--sample_idxes', default=[0, 100])
-    parser.add_argument('--dir_root_libsvm', default=r'C:\Users\admin\Desktop\demo\libsvm')
-    parser.add_argument('--dev_ser', default='COM3')
-    parser.add_argument('--sample_idxes', default=[])
+    parser.add_argument('--dir_root_libsvm', default='/home/manu/nfs/libsvm')
+    parser.add_argument('--dev_ser', default='/dev/ttyUSB0')
+    parser.add_argument('--sample_idxes', default=[0, 100])
+    # parser.add_argument('--dir_root_libsvm', default=r'C:\Users\admin\Desktop\demo\libsvm')
+    # parser.add_argument('--dev_ser', default='COM3')
+    # parser.add_argument('--sample_idxes', default=[])
+    parser.add_argument('--save_dir', default='/home/manu/tmp')
     return parser.parse_args()
 
 
@@ -31,7 +32,7 @@ def main():
         if len(args.sample_idxes) > 0 and \
                 args.addrs_sensor[0] in smoke_detector.db.keys() and \
                 smoke_detector.db[args.addrs_sensor[0]].get_seq_len() >= args.sample_idxes[-1]:
-            smoke_detector.save_db(args.addrs_sensor, args.sample_idxes)
+            smoke_detector.save_db(args.addrs_sensor, args.sample_idxes, save_dir=args.save_dir)
             break
 
 

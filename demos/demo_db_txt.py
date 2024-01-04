06db13e6671f7c2f5e6779ccb1f422f3e3799032
manu <zxthz@126.com>
2024-01-04 13:33:50 +0800

    file sorting

--- demos/demo_db_txt.py
+++ demos/demo_db_txt.py
@@ -8,7 +8,7 @@ from utils import set_logging, db_gen_v3
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_in',
-                        default='/media/manu/data/docs/smokes/data_202312/尖嘴加湿器/3.txt')
+                        default='/media/manu/data/docs/smokes/data_202312/尖嘴加湿器/18.txt')
     parser.add_argument('--dir_root_libsvm', default='/home/manu/nfs/libsvm')
     parser.add_argument('--addrs_sensor', default=[f'1_{1}'])
     return parser.parse_args()

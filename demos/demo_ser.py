cbfad08c84ac19a471230691bfe9694eb4c36b0b
manu <zxthz@126.com>
2024-03-12 10:12:32 +0800

    tiny

--- demos/demo_ser.py
+++ demos/demo_ser.py
@@ -8,7 +8,7 @@ from utils.utils import set_logging
 def parse_args():
     parser = argparse.ArgumentParser()
     # parser.add_argument('--addrs_sensor', default=[f'1_{1}', f'1_{5}'])
-    parser.add_argument('--addrs_sensor', default=[f'1_{2}'])
+    parser.add_argument('--addrs_sensor', default=[f'1_{1}'])
     # parser.add_argument('--dir_root_libsvm', default='/home/manu/nfs/libsvm')
     # parser.add_argument('--dev_ser', default='/dev/ttyUSB0')
     # parser.add_argument('--save_dir', default='/home/manu/tmp')
